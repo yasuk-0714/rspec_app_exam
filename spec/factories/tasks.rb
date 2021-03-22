0998c801b47a53b916a5c951892ceea1529bd108
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-03-22 11:25:58 +0900

    04_テストコード修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
   factory :task do
-    title { 'Task' }
+    title { 'SampleTask' }
     status { rand(2) }
     from = Date.parse("2019/08/01")
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
+    association :project
+
+    trait :another_task do
+      status { :done }
+      completion_date { Time.current.yesterday }
+    end
   end
 end
