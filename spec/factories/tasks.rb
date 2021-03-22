9ae83e25a2d9178f2abec5bc810b743a6648f1d4
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-03-22 13:09:16 +0900

    Fix: factoryデータの修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
-    title { 'SampleTask' }
+    title { 'Task' }
     status { rand(2) }
     from = Date.parse("2019/08/01")
     to   = Date.parse("2019/12/31")
