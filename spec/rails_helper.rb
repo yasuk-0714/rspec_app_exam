76f8ee9f7085f410def5b344dcf1856daea401c2
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-03-22 12:54:17 +0900

    Fix: includeの記載位置を変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,6 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  include ApplicationHelper
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -59,4 +58,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  include ApplicationHelper
 end
